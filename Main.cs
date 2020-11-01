2ad6adb3ad1a752386ac5e8a98ccfcfcbd4b95a8
rominat0r <57713747+rominat0r@users.noreply.github.com>
2020-11-01 23:57:04 +0100

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static microsoft.botsay.Program;
+
+
+namespace microsoft.botsay
+{
+	public class Main1
+	{
+		public static void Main(string[] argv)
+		{
+			Program.MainProgram(argv);
+		}
+	}
+}
